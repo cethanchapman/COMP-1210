792f6743bf96f5301dc3ae3ceb5560ab8e4f6e51
cethanchapman <100757494+cethanchapman@users.noreply.github.com>
2024-02-07 12:17:53 -0600

    Create StudentInfo.java

---
+++ activities/activity1/StudentInfo.java
@@ -0,0 +1,19 @@
+/** Simple program to print student information.
+*
+* Activity 1
+* @author Ethan Chapman- Comp 1210 - 001
+* @version 01/18/22
+*/
+public class StudentInfo
+{
+ /** prints student information once.
+ * @param args Command line arguments - not used.
+ */
+ 
+   public static void main(String[]args)
+   {
+      System.out.println("Name: Ethan Chapman");
+      System.out.println("Previous Computer Courses:"); 
+      System.out.println("   Introductory Python course");
+   }
+}
