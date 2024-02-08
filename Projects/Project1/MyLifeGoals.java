af4d7ed27c4a4235a72080174a756acdce285799
cethanchapman <100757494+cethanchapman@users.noreply.github.com>
2024-02-07 21:39:21 -0600

    Create MyLifeGoals.java

---
+++ Projects/Project1/MyLifeGoals.java
@@ -0,0 +1,27 @@
+/** Simple program to print my life goals.
+*
+* Project 01
+* @author Ethan Chapman - Comp 1210 - 001
+* @version 01/19/22
+*/
+public class MyLifeGoals
+{
+ /** prints student's life goals once.
+ * @param args Command line arguments - not used
+ */
+   
+   public static void main(String[]args)
+   {
+      System.out.println("Ethan Chapman");
+      System.out.println();
+      System.out.println("I want to get at least a 3.0 GPA this semester." 
+         + " I will need to work hard every day and keep a decent sleep" 
+         + " scedule. I will need to make sure I have good time management.");
+      System.out.println("I want to continue studying at Auburn and maintain"
+         + " my scholarship throughout my years here. I also want to improve" 
+         + " my coding ability and software knowledge.");
+      System.out.println("I want to graduate from Auburn with a bachelor's" 
+         + " degree in computer science. I then want to attend graduate school"
+         + " to pursue higher education.");
+   }
+}
