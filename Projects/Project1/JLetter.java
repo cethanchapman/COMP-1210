259b1108d29b7c0ca038972d5f7adff2d1526065
cethanchapman <100757494+cethanchapman@users.noreply.github.com>
2024-02-07 21:38:30 -0600

    Create JLetter.java

---
+++ Projects/Project1/JLetter.java
@@ -0,0 +1,26 @@
+/** Simple program to print my life goals.
+*
+* Project 01
+* @author Ethan Chapman - Comp 1210 - 001
+* @version 01/19/22
+*/
+public class JLetter
+{
+ /** prints student's life goals once.
+ * @param args Command line arguments - not used
+ */
+   
+   public static void main(String[]args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("J     JAVA  ");
+      System.out.println("JA    JAVA  ");
+      System.out.println(" JAVAJAVA   ");
+      System.out.println("  JAVAJA    ");
+   }
+}
